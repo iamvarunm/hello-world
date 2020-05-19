7332e53173d07ce6f516e80a203862126e6f0f59
Varun Mishra <65210091+iamvarunm@users.noreply.github.com>
2020-05-19 09:20:29 +0530

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -3,7 +3,7 @@
 #1) Please put your Name, About You, etc here
 
 Name = "Varun"
-Bio = "I am a Data Science Enthusiast. I love to work with Python, R, SAS and Tableau. Have knowledge of Microsot Excel, Power BI also."
+Bio = "I am a Data Science Enthusiast. I love to work with Python, R, SAS and Tableau. Have knowledge of Microsoft Excel, Power BI also."
 Profile = "https://www.linkedin.com/in/iamvarunm/"
 
 #2) Code starts from here (Python is a case-sensitive language. This means, Variable and variable are not the same. Always give the identifiers a name that makes sense)
